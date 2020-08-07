31bb5064e47c95589cb934caf046a192193c3e5b
Nichlas Simon Vedel Poulsen <nich1840@zbc.dk>
2020-08-07 12:26:03 +0200

    push

---
+++ Metoder/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> iRandList = new List<int>();
+
+            Console.WriteLine("Noget text");
+            RandList(iRandList);
+
+            Console.WriteLine("\nNoget text");
+            BubbleSort(iRandList);
+
+            Console.WriteLine("\nNoget mere text");
+            CheckIfSorted(iRandList);
+
+        }
+
+        public static List<int> BubbleSort(List<int> tmplist)
+        {
+            int tmp;
+
+            for (int i = 0; i < tmplist.Count; i++)
+            {
+                for (int j = 0; j < (tmplist.Count - 1) - i; j++)
+                {
+                    if (tmplist[j] > tmplist[j + 1])
+                    {
+                        tmp = tmplist[j + 1];
+                        tmplist[j + 1] = tmplist[j];
+                        tmplist[j] = tmp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < tmplist.Count; i++)
+            {
+                Console.WriteLine(tmplist[i]);
+            }
+
+            return tmplist;
+        }
+
+        public static List<int> RandList(List<int> aRandList)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < 7; i++)
+            {
+                int randNum = rand.Next(1, 199);
+                aRandList.Add(randNum);
+            }
+
+            for (int i = 0; i < aRandList.Count; i++)
+            {
+                Console.WriteLine(aRandList[i]);
+            }
+
+            return aRandList;
+        }
+
+        public static bool CheckIfSorted (List<int> tmplist)
+        {
+            int tmpCheck = 0;
+            bool check;
+
+            for (int i = 0; i < tmplist.Count; i++)
+            {
+                if (tmplist[i] >= tmplist[i])
+                {
+                    tmpCheck++;
+                }
+            }
+
+            if (tmplist.Count == tmpCheck)
+            {
+                check = true;
+            }
+            else
+            {
+                check = false;
+            }
+
+            Console.WriteLine(check);
+
+            return check;
+        }
+
+    }
+
+}
